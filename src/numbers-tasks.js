3f7c2723e53eb81df94fa77ceedee28a4943385a
polyakov-artem <polyakov.contact@yandex.ru>
2024-11-01 21:26:58 +0300

    feat: implement roundToNearestInteger, getIntegerPartNumber, getSumOfNumbers, getMaxNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -525,8 +525,8 @@ function roundToLargestInteger(number) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function roundToNearestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToNearestInteger(number) {
+  return Math.round(number);
 }
 
 /**
@@ -540,8 +540,8 @@ function roundToNearestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function getIntegerPartNumber(/* number */) {
-  throw new Error('Not implemented');
+function getIntegerPartNumber(number) {
+  return parseInt(`${number}`, 10);
 }
 
 /**
@@ -556,8 +556,8 @@ function getIntegerPartNumber(/* number */) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(...args) {
+  return args.reduce((total, cur) => Number((total + cur).toFixed(1)), 0);
 }
 
 /**
@@ -572,8 +572,8 @@ function getSumOfNumbers(/* x1, x2, x3 */) {
  * -5, -6 => -5
  * 0, 5   => 5
  */
-function getMaxNumber(/* firstNumber, secondNumber */) {
-  throw new Error('Not implemented');
+function getMaxNumber(firstNumber, secondNumber) {
+  return Math.max(firstNumber, secondNumber);
 }
 
 /**
